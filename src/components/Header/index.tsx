4559367bc237adbe12890e048c00d16b8c51a35d
Monett <monet9812@gmail.com>
2023-01-07 23:17:30 +0900

    feat: google login

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,34 @@
 import Link from "next/link";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { app } from "src/utils/firebase";
+import { useState } from "react";
 
 export default function Header() {
+  const [userName, setUserName] = useState("");
+
+  const onClickLogin = () => {
+    const provider = new GoogleAuthProvider();
+    const auth = getAuth(app);
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const credential = GoogleAuthProvider.credentialFromResult(result);
+        const user = result.user;
+        console.log(user);
+        setUserName(user.displayName);
+        if (credential) {
+          const token = credential.accessToken;
+          console.log(token);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        const email = error.customData.email;
+        const credential = GoogleAuthProvider.credentialFromError(error);
+      });
+  };
+
   return (
     <header className="flex flex-row justify-between p-10 bg-sky-300">
       <nav>
@@ -10,7 +38,11 @@ export default function Header() {
           </li>
         </ul>
       </nav>
-      <button>로그인</button>
+      {userName != "" ? (
+        <div>{userName}님</div>
+      ) : (
+        <button onClick={() => onClickLogin()}>로그인</button>
+      )}
     </header>
   );
 }
