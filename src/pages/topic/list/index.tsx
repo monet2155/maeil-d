05b007c729d93c65d612fee8e24ce73fefe58769
Monett <monet9812@gmail.com>
2023-01-07 22:42:49 +0900

    feat: topic list set head

--- src/pages/topic/list/index.tsx
+++ src/pages/topic/list/index.tsx
@@ -1,3 +1,17 @@
+import Head from "next/head";
+
 export default function TopicListPage() {
-  return <div>주제 목록</div>;
+  return (
+    <>
+      <Head>
+        <title>매일디</title>
+        <meta name="description" content="매일디" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <div>주제 목록</div>
+      </main>
+    </>
+  );
 }
