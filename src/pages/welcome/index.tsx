6249425fb2aa41312d9b5256a619ebcd6e3e2fc8
Monett <monet9812@gmail.com>
2023-02-05 01:33:31 +0900

    feat: MID-48 로그인 후 약관 확인

--- src/pages/welcome/index.tsx
+++ src/pages/welcome/index.tsx
@@ -2,6 +2,7 @@ import { createUser } from "@apis/users";
 import { userAtom } from "@store";
 import { useAtom } from "jotai";
 import Head from "next/head";
+import Link from "next/link";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
@@ -55,7 +56,34 @@ export default function MyPage() {
             onChange={(e) => setDisplayName(e.target.value)}
           />
         </section>
-        <button onClick={onClickSubmit}>확인</button>
+        <section className="my-4">
+          <p>닉네임은 한번 설정하면 변경할 수 없습니다.</p>
+          <p>닉네임은 2~10자 사이로 설정해주세요.</p>
+        </section>
+        <section className="my-4">
+          <p>
+            확인 버튼을 누르면 매일디의{" "}
+            <Link
+              href="/terms"
+              target="_blank"
+              className="text-blue-700 underline"
+            >
+              이용약관
+            </Link>
+            ,{" "}
+            <Link
+              href="https://plip.kr/html/78f76b2b-01ee-4d54-b724-9520a5d34a60.html"
+              target="_blank"
+              className="text-blue-700 underline"
+            >
+              개인정보처리방침
+            </Link>
+            에 동의하게 됩니다.
+          </p>
+        </section>
+        <button onClick={onClickSubmit} className="p-4 border rounded">
+          확인
+        </button>
       </main>
     </>
   );
