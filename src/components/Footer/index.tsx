8660e051022731faf3d0ce92123ba8e0af808440
Monett <monet9812@gmail.com>
2023-02-07 22:33:16 +0900

    feat: footer basic functions and layout

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,34 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
-    <footer>
-      <p>Footer</p>
+    <footer className="flex flex-col p-10 bg-gray-200">
+      <section className="flex flex-row gap-8">
+        <section>
+          <h1>매일디</h1>
+          <h2 className="text-xs">
+            Empowering creativity, one design at a time:
+            <br /> Welcome to 매일디, the daily hub for designers and design
+            enthusiasts.
+          </h2>
+        </section>
+        <section className="flex flex-col">
+          <h1 className="font-bold">Company</h1>
+          <Link href="/">서비스소개</Link>
+          <Link href="/terms">이용약관</Link>
+          <Link
+            href="https://plip.kr/html/78f76b2b-01ee-4d54-b724-9520a5d34a60.html"
+            target="_blank"
+          >
+            개인정보처리방침
+          </Link>
+        </section>
+      </section>
+      <div className="h-[1px] my-6 bg-gray-300" />
+      <section className="flex flex-row justify-between">
+        <section>© 2023 로망난녀석들. All rights reserved.</section>
+        <section>0 designs and 0 topics in 매일디</section>
+      </section>
     </footer>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className="h-[80vh]">
         <div>매일디에 오신 것을 환영합니다.</div>
       </main>
     </>
