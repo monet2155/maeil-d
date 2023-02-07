50092d4658ca42c929c89346535f5d83db1bdd43
Monett <monet9812@gmail.com>
2023-02-07 22:08:55 +0900

    fix: get ga move to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import "../styles/globals.css";
+import { getAnalytics } from "firebase/analytics";
+import { useEffect } from "react";
+import { app } from "src/utils/firebase";
 import type { AppProps } from "next/app";
 import Header from "@components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    const analytics = getAnalytics(app);
+  }, []);
+
   return (
     <>
       <Header />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,6 @@
-import Header from "@components/Header";
-import { getAnalytics } from "firebase/analytics";
 import { Html, Head, Main, NextScript } from "next/document";
-import { useEffect } from "react";
-import { app } from "src/utils/firebase";
 
 export default function Document() {
-  useEffect(() => {
-    const analytics = getAnalytics(app);
-  }, []);
   return (
     <Html lang="en">
       <Head />
