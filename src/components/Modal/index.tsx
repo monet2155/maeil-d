b81b33d1fcae5e8ef764fb753762004e530c850c
Monett <monet9812@gmail.com>
2023-03-23 00:52:43 +0900

    fix: basic modal onclickconfirm

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -58,6 +58,7 @@ export default function Modal(props: ModalProps) {
             <div className="flex items-center justify-end p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
               {onClickConfirm && (
                 <button
+                  onClick={onClickConfirm}
                   type="button"
                   className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                 >
