8716cb1c80892489aaee25ca5e4ed40382d2928c
Monett <monet9812@gmail.com>
2023-04-12 21:55:03 +0900

    feat: repub theme list - theme card

--- src/pages/theme/list/index.tsx
+++ src/pages/theme/list/index.tsx
@@ -4,9 +4,14 @@ import Head from "next/head";
 import { getThemeList } from "@apis/themes";
 import { Theme } from "src/@types/theme";
 import Link from "next/link";
+import { getDesignListByThemeId } from "@apis/designs";
 
 export default function ThemeListPage() {
   const [themes, setThemes] = useState<Theme[]>([]);
+  const [selectedTheme, setSelectedTheme] = useState<Theme | null>(null);
+
+  const [uploaderCount, setUploaderCount] = useState(0);
+  const [designCount, setDesignCount] = useState(0);
 
   useEffect(() => {
     getThemeList()
@@ -20,6 +25,24 @@ export default function ThemeListPage() {
       .catch((err) => console.log(err));
   }, []);
 
+  useEffect(() => {
+    if (themes.length > 0) {
+      setSelectedTheme(themes[0]);
+    }
+  }, [themes]);
+
+  useEffect(() => {
+    if (!selectedTheme) return;
+    getDesignListByThemeId(selectedTheme.id).then((res) => {
+      const uploaderSet = new Set();
+      res.docs.forEach((doc) => {
+        uploaderSet.add(doc.data().uploader);
+      });
+      setUploaderCount(uploaderSet.size);
+      setDesignCount(res.docs.length);
+    });
+  }, [selectedTheme]);
+
   return (
     <>
       <Head>
@@ -28,25 +51,28 @@ export default function ThemeListPage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <div>주제 목록</div>
-        <div>
-          <ul className="flex flex-row flex-wrap gap-2 p-4">
-            {themes.map((theme) => (
-              <li key={theme.id} className="rounded-lg shadow-lg">
-                <Link className="flex p-5" href={`/theme/detail/${theme.id}`}>
-                  <div className="flex flex-col">
-                    <h1>{theme.name}</h1>
-                    <div className="my-4 h-[1px] bg-black" />
-                    <div className="max-w-xs min-w-[20rem] ">
-                      {theme.description}
-                    </div>
-                  </div>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </div>
+      <main className="min-h-[1000px] flex flex-row pb-8">
+        <section
+          className="w-[1280px] bg-cover bg-center bg-no-repeat p-8"
+          style={{
+            backgroundImage: `url(${
+              selectedTheme ? selectedTheme.thumbnailUrl : ""
+            })`,
+          }}
+        >
+          <h1 className="text-white font-bold text-[40px] leading-none tracking-[0.02em]">
+            {selectedTheme?.name}
+          </h1>
+          <h2 className="text-white text-[20px] leading-none tracking-[0.02em] my-2">
+            현재 제출자 {uploaderCount}명
+          </h2>
+          <h2 className="text-white text-[20px] leading-none tracking-[0.02em]">
+            현재 등록된 디자인 {designCount > 99 ? "99+" : designCount} 개
+          </h2>
+          <h3 className="text-white text-[24px] leading-normal tracking-[0.04em] max-w-[691px] mt-4">
+            {selectedTheme?.description}
+          </h3>
+        </section>
       </main>
     </>
   );
