8164027f85e609ffe959cd2c4e1986536b061ab1
Monett <monet9812@gmail.com>
2023-05-31 23:04:13 +0900

    feat: ssr on main page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,35 +1,14 @@
-import {
-  getDesignList,
-  getDesignListByUserId,
-  subscribeDesignCount,
-} from "@apis/designs";
-import { getThemeList, subscribeThemeCount } from "@apis/themes";
+import { getDesignList } from "@apis/designs";
 import DesignItem from "@components/DesignItem";
-import MainThemeItem from "@components/MainThemeItem";
-import SignatureToken from "@components/SignatureToken";
-import cn from "classnames";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import Head from "next/head";
-import Link from "next/link";
-import { useEffect, useRef, useState } from "react";
+import { useState } from "react";
 import { Design } from "src/@types/design";
-import { Theme } from "src/@types/theme";
 
-export default function Home() {
-  const [designList, setDesignList] = useState<Design[]>([]);
-
-  useEffect(() => {
-    getDesignList("recent")
-      .then((snapshot) => {
-        let designList: Design[] = [];
-        snapshot.forEach((doc) => {
-          designList.push({ ...(doc.data() as Design), id: doc.id });
-        });
-        setDesignList(designList);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
+export default function Home({
+  initialDesignList,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  const [designList, setDesignList] = useState<Design[]>(initialDesignList);
 
   return (
     <>
@@ -54,3 +33,20 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps<{
+  initialDesignList: Design[];
+}> = async () => {
+  const designList = await getDesignList("recent");
+
+  const initialDesignList = designList.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  })) as Design[];
+
+  return {
+    props: {
+      initialDesignList: JSON.parse(JSON.stringify(initialDesignList)),
+    },
+  };
+};
