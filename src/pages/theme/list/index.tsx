c01c1bb81bba1b4b4e01e1cc4ce78990e409618d
Monett <monet9812@gmail.com>
2023-06-01 22:06:15 +0900

    fix: reset theme list page

--- src/pages/theme/list/index.tsx
+++ src/pages/theme/list/index.tsx
@@ -2,17 +2,9 @@ import { useEffect, useState } from "react";
 import Head from "next/head";
 import { getThemeList } from "@apis/themes";
 import { Theme } from "src/@types/theme";
-import { getDesignListByThemeId } from "@apis/designs";
-import cn from "classnames";
-import Link from "next/link";
-import ThemeCard from "@components/ThemeCard";
 
 export default function ThemeListPage() {
   const [themeList, setThemeList] = useState<Theme[]>([]);
-  const [selectedTheme, setSelectedTheme] = useState<Theme | null>(null);
-
-  const [uploaderCount, setUploaderCount] = useState(0);
-  const [designCount, setDesignCount] = useState(0);
 
   useEffect(() => {
     getThemeList()
@@ -26,24 +18,6 @@ export default function ThemeListPage() {
       .catch((err) => console.log(err));
   }, []);
 
-  useEffect(() => {
-    if (themeList.length > 0) {
-      setSelectedTheme(themeList[0]);
-    }
-  }, [themeList]);
-
-  useEffect(() => {
-    if (!selectedTheme) return;
-    getDesignListByThemeId(selectedTheme.id).then((res) => {
-      const uploaderSet = new Set();
-      res.docs.forEach((doc) => {
-        uploaderSet.add(doc.data().uploader);
-      });
-      setUploaderCount(uploaderSet.size);
-      setDesignCount(res.docs.length);
-    });
-  }, [selectedTheme]);
-
   return (
     <>
       <Head>
@@ -53,48 +27,7 @@ export default function ThemeListPage() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="min-h-[1000px] flex flex-row pb-8">
-        <ThemeCard
-          selectedTheme={selectedTheme}
-          uploaderCount={uploaderCount}
-          designCount={designCount}
-        />
-        <section className="max-h-[1000px] w-full overflow-auto scrollbar-hide">
-          <ul>
-            {themeList.map((theme, index) => (
-              <>
-                {selectedTheme?.id !== theme.id ? (
-                  <li
-                    key={theme.id}
-                    className={cn(
-                      "h-[128px] p-6 border-t border-[#1d1d1d] cursor-pointer",
-                      {
-                        "border-b": index === themeList.length - 1,
-                      }
-                    )}
-                    onClick={() => setSelectedTheme(theme)}
-                  >
-                    <h1 className=" font-bold text-[24px] leading-none tracking-[0.04em] text-[#1d1d1d]">
-                      {theme.name}
-                    </h1>
-                    <h3 className="mt-2 text-[16px] leading-normal tracking-[0.04em] text-[#1d1d1d]">
-                      {theme.description}
-                    </h3>
-                  </li>
-                ) : (
-                  <Link
-                    key={theme.id}
-                    className="h-[128px] bg-[#1d1d1d] cursor-pointer flex items-center justify-center"
-                    href={`/theme/detail/${theme.id}`}
-                  >
-                    <h1 className="text-white font-bold text-[24px] leading-none tracking-[0.04em]">
-                      상세 확인 &gt;
-                    </h1>
-                  </Link>
-                )}
-              </>
-            ))}
-          </ul>
-        </section>
+        <section className=""></section>
       </main>
     </>
   );
